fe835efa1abb8493b7ee5c8d56ed9363c0499946
Alireza Haghighi <alireza.haghighi1224@gmail.com>
2024-10-18 22:53:34 +0330

    added drop down

--- src/Components/header/HeaderForm.jsx
+++ src/Components/header/HeaderForm.jsx
@@ -3,10 +3,16 @@ import Buttons from "../../Components/buttons/Buttons";
 import { Link } from "react-router-dom";
 
 export default function HeaderForm() {
+  const username = "jese Leos";
+  const email = "name@example";
+
   return (
     <div>
       {/* header */}
-      <div className="flex justify-between h-16" style={{ background: "#374151" }}>
+      <div
+        className="flex justify-between h-16"
+        style={{ background: "#1F2A37" }}
+      >
         {/* right side */}
         <div className="flex items-center">
           <Link>
@@ -46,7 +52,6 @@ export default function HeaderForm() {
               </span>
             }
           />
-          
 
           <img
             src="/icons/Notification Icon.png"
@@ -54,11 +59,37 @@ export default function HeaderForm() {
             className="mx-3 cursor-pointer"
           />
 
-          <img
-            src="/images/7f9adfb6486c67063d80474f4d89a506.jpg"
-            alt="Profile"
-            className="w-8 h-8 rounded-full ml-4 cursor-pointer"
-          />
+          <details>
+            <summary>
+              <img
+                src="/images/7f9adfb6486c67063d80474f4d89a506.jpg"
+                alt="Profile"
+                className="w-8 h-8 rounded-full ml-4 cursor-pointer"
+              />
+            </summary>
+            <ul>
+              <li>
+                <span>{username}</span>
+                <span className="email-info">{email}</span>
+              </li>
+              <li>
+                <Link to={"/my-account"} />
+                حساب من
+              </li>
+              <li>
+                <Link to={"/account-lists"} />
+                لیست حساب ها
+              </li>
+              <li>
+                <Link to={"/logout"} />
+                خروج
+              </li>
+              <li>
+                <Link to={"/delete-account"} />
+                حذف حساب{" "}
+              </li>
+            </ul>
+          </details>
         </div>
       </div>
     </div>
