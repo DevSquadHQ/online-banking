da822830d89a6ae685f048007460af089f161670
Alireza Haghighi <alireza.haghighi1224@gmail.com>
2024-10-18 07:10:36 +0330

    added login page

--- src/Pages/signUp/SignupForm.jsx
+++ src/Pages/signUp/SignupForm.jsx
@@ -8,46 +8,49 @@ export default function Signup() {
   return (
     <Container>
       {/* whole form */}
-      <div className="flex justify-center items-center h-screen ">
-        <div className="w-96">
-          <h2 className="text-white text-center mb-8 text-2xl font-bold">
+      <div className='flex justify-center items-center h-screen '>
+        {/* form-title */}
+        <div className='w-96'>
+          <h2 className='text-white text-center mb-8 text-2xl font-bold'>
             اینترنت بانک من
           </h2>
+
+          {/* form-content */}
           <form
             style={{ background: "#1F2A37" }}
-            className="px-5 py-6 rounded-lg"
+            className='px-5 py-6 rounded-lg'
           >
-            <h3 className="text-white mb-6 font-semibold">ایجاد حساب کاربری</h3>
+            <h3 className='text-white mb-6 font-semibold'>ایجاد حساب کاربری</h3>
 
             <Input
               style={{ background: "#374151" }}
-              className="my-2 py-2 px-2 w-full rounded-md text-gray-300 border-spacing-1 border-gray-300 outline-none"
-              inputName="نام"
-              type="text"
-              placeholder="لطفا نام  خود را وارد کنید"
+              className='my-2 py-2 px-2 w-full rounded-md text-gray-300 border-spacing-1 border-gray-300 outline-none'
+              inputName='نام'
+              type='text'
+              placeholder='لطفا نام  خود را وارد کنید'
             />
 
             <Input
               style={{ background: "#374151" }}
-              className="my-2 py-2 px-2 w-full rounded-md text-gray-300 border-spacing-1 border-gray-300 outline-none"
+              className='my-2 py-2 px-2 w-full rounded-md text-gray-300 border-spacing-1 border-gray-300 outline-none'
               inputName={"نام خانوادگی"}
-              type="text"
-              placeholder=" لطفا نام خانوادگی خود را وارد کنید"
+              type='text'
+              placeholder=' لطفا نام خانوادگی خود را وارد کنید'
             />
 
             <Input
               style={{ background: "#374151" }}
-              className="my-2 py-2 px-2 w-full rounded-md text-gray-300 border-spacing-1 border-gray-300 outline-none"
+              className='my-2 py-2 px-2 w-full rounded-md text-gray-300 border-spacing-1 border-gray-300 outline-none'
               inputName={"کدملی"}
-              type="number"
-              placeholder=" لطفا کدملی خود را وارد کنید"
+              type='number'
+              placeholder=' لطفا کدملی خود را وارد کنید'
             />
-            <div>
-              <Buttons btnName={"ادامه"} />
-            </div>
-            <p className="text-gray-400">
+            <Buttons btnName={"ادامه"} />
+
+            {/* form-subtext */}
+            <p className='text-gray-400'>
               حساب کاربری دارید؟{" "}
-              <Link to={"/"} className="text-blue-700">
+              <Link to={"/login"} className='text-blue-700'>
                 ورود به حساب
               </Link>
             </p>
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,11 @@
-import Signup from "./Pages/signUp/SignupForm"
-
-import Home from "./Pages/Home/Home"
-
+import Home from "./Pages/Home/Home";
+import Signup from "./Pages/signUp/SignupForm";
+import LoginPage from "./Pages/LoginPage";
 
 let routes = [
-    
-    {path:'/', element:<Home/>},
-    {path:'/signup', element:<Signup/>},
-]
+  { path: "/", element: <Home /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/login", element: <LoginPage /> },
+];
 
-export default routes
+export default routes;
