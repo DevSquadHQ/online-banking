163988695e5864c958a91a965e13479e4a5ccfb4
Reza <rtajadod93@gmail.com>
2024-10-19 10:50:20 +0200

    semi complete sidebar

--- src/Components/sidebar/SidebarForm.jsx
+++ src/Components/sidebar/SidebarForm.jsx
@@ -1,40 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SidebarForm() {
+  const [expanded, setExpanded] = useState(true);
+  const clickHandler = () => setExpanded((makeOpen) => !makeOpen);
   return (
     <div
       style={{ background: "#374151" }}
-      className="w-[275px] text-white h-screen px-5 pt-5"
+      className={` text-white h-screen pt-5 transition-all duration-300 ${
+        expanded ? "w-[275px] px-5" : "w-0 px-0"
+      }`}
     >
       <ul
-        className="[&>*]:flex space-y-6  [&>img]:ml-10 border-b-2 border-gray-600"
-        
+        className={`[&>*]:flex space-y-6  [&>img]:ml-10 border-b-2 border-gray-600 [&>li:hover]:bg-slate-400 [&>li:hover]:transition-all [&>li:hover]:delay-75`}
       >
         <li>
-          <img src="/icons/document-report (1).png" className="ml-2" />
-          خانه
+          <img src="/icons/document-report (1).png" className={`overflow-hidden transition-all ${expanded ? 'w-6 ml-2' : 'w-0'}`} />
+          <span className={`overflow-hidden transition-all ${expanded ? 'w-52' : 'w-0'}`}>خانه</span>
         </li>
         <li>
-          <img src="/icons/chart-pie.png" className="ml-2" />
-          گزارش
+          <img src="/icons/chart-pie.png" className={`overflow-hidden transition-all ${expanded ? 'w-6 ml-2' : 'w-0'}`}/>
+          <span className={`overflow-hidden transition-all ${expanded ? 'w-52' : 'w-0'}`}>گزارش</span>
         </li>
         <li>
-          <img src="/icons/inbox-in.png" className="ml-2" />
-          انتقال وجه
+          <img src="/icons/inbox-in.png" className={`overflow-hidden transition-all ${expanded ? 'w-6 ml-2' : 'w-0'}`} />
+          <span className={`overflow-hidden transition-all ${expanded ? 'w-52' : 'w-0'}`}>انتقال وجه</span>
         </li>
 
         <li className="flex justify-between items-center">
           {" "}
           <span className="flex ">
             {" "}
-            <img src="/icons/shopping-bag.png" className="ml-2" />
-            تنظیمات{" "}
+            <img src="/icons/shopping-bag.png" className={`overflow-hidden transition-all ${expanded ? 'w-6 ml-2' : 'w-0'}`} />
+            <span className={`overflow-hidden transition-all ${expanded ? 'w-52' : 'w-0'}`}>تنظیمات </span>
           </span>
           <img src="/icons/chevron-up.png" className="ml-2" />
         </li>
-        <li className="mr-8">مسدود سازی</li>
-        <li className="mr-8 pb-8">تغییر رمز اولیه ورود</li>
+        
+        <li className="mr-8 invisible">مسدود سازی</li>
+        <li className="mr-8 pb-8 invisible">تغییر رمز اولیه ورود</li>
       </ul>
+
+      <button onClick={clickHandler}>click</button>
     </div>
   );
 }
