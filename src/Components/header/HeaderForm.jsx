c9c2f243d179da22b1515d0f946bb2219c1e646b
Alireza Haghighi <alireza.haghighi1224@gmail.com>
2024-10-19 11:52:06 +0330

    made dropdown a component

--- src/Components/header/HeaderForm.jsx
+++ src/Components/header/HeaderForm.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Buttons from "../../Components/buttons/Buttons";
 import { Link } from "react-router-dom";
+import DropDown from "../links/drop-down/DropDown";
 
 export default function HeaderForm() {
-  const username = "jese Leos";
-  const email = "name@example";
-
   return (
     <div>
       {/* header */}
@@ -58,38 +56,7 @@ export default function HeaderForm() {
             alt="Notification Icon"
             className="mx-3 cursor-pointer"
           />
-
-          <details>
-            <summary>
-              <img
-                src="/images/7f9adfb6486c67063d80474f4d89a506.jpg"
-                alt="Profile"
-                className="w-8 h-8 rounded-full ml-4 cursor-pointer"
-              />
-            </summary>
-            <ul>
-              <li>
-                <span>{username}</span>
-                <span className="email-info">{email}</span>
-              </li>
-              <li>
-                <Link to={"/my-account"} />
-                حساب من
-              </li>
-              <li>
-                <Link to={"/account-lists"} />
-                لیست حساب ها
-              </li>
-              <li>
-                <Link to={"/logout"} />
-                خروج
-              </li>
-              <li>
-                <Link to={"/delete-account"} />
-                حذف حساب{" "}
-              </li>
-            </ul>
-          </details>
+          <DropDown username={"jese Leos"} email={"name@example"} />
         </div>
       </div>
     </div>
