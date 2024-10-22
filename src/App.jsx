49c22336e1e388b2fd0d0041bb504c88c023e74f
Alireza Haghighi <alireza.haghighi1224@gmail.com>
2024-10-22 19:34:48 +0330

    update-user-dropdown
    changed it to a component (can get username & email),
    added a transition (works only once),
    will be closed by clicking outside.

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,15 @@ export default function App() {
   let router = useRoutes(routes);
   let location = useLocation();
 
+  /* select all the elements of details tag that is for top left icon of the user and stores it in the 'allDropdown' variable. */
+  const allDropdowns = document.querySelectorAll(".user-dropdown");
+
+  /* sets up an event listener for click events on the whole window */
+  window.addEventListener("click", () => {
+    /* when the window detects a click (whenever you click anywhere), this code runs. It converts 'allDropdown' nodelist into an array and then loop through each one to sets the 'open' attribute to 'false'. */
+    [...allDropdowns].forEach((dropdown) => (dropdown.open = false));
+  });
+
   return (
     <div>
       {location.pathname === "/home" ? (
