70d61490b72770e907dd6e15854b4da58c2ee72b
Reza <rtajadod93@gmail.com>
2024-10-23 10:50:59 +0200

    tiny update on password input

--- src/Pages/loginPage/Login.jsx
+++ src/Pages/loginPage/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import LoginPage from './LoginForm'
+// import LoginPage from './LoginForm'
+import LoginForm from './LoginForm'
 
 export default function Login() {
   return (
     <div>
-        <LoginPage/>
+        <LoginForm/>
     </div>
   )
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,15 +3,17 @@ import AccountDetail from "./Components/accountDetail/accountDetail";
 import Signup from "./Pages/signUp/signUp1/SignUp";
 import Home from "./Pages/Home/Home";
 import SignUp2 from "./Pages/signUp/signUp2/signUp2";
+import Login from "./Pages/loginPage/Login";
 
 // Define routes array first
 let routes = [
-    { path: '/home', element: <Home /> },
-    { path: '/signup', element: <Signup /> },
-    { path: '/login', element: <LoginPage /> }, 
-    { path: '/signup2', element: <SignUp2 /> },
-    { path: "/detail", element: <AccountDetail /> },
-    { path: "/", element: <LoginPage /> },
+  { path: "/home", element: <Home /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/login", element: <Login /> },
+
+  { path: "/signup2", element: <SignUp2 /> },
+  { path: "/detail", element: <AccountDetail /> },
+  { path: "/", element: <LoginPage /> },
 ];
 
 // Export routes
