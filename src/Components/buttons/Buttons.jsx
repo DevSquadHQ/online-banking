9989f978b7284beea8fac431ddd74e591ac8a541
Reza <rtajadod93@gmail.com>
2024-10-23 10:37:13 +0200

    solving some bugs

--- src/Components/buttons/Buttons.jsx
+++ src/Components/buttons/Buttons.jsx
@@ -4,12 +4,10 @@ import { Link } from "react-router-dom";
 export default function Buttons(props) {
   const { btnName, to, className } = props;
   return (
-        className={ `${className}
-          text-white w-full  my-2 py-2 px-2 rounded-md bg-blue-600 hover:bg-blue-700 transition-all ease-in-out delay-200
-        `}
-      >
-      <button
     <Link to={to}>
+      <button
+        className={`${className} text-white w-full my-2 py-2 px-2 rounded-md bg-blue-600 hover:bg-blue-700 transition-all ease-in-out delay-200`}
+      >
         {btnName}
       </button>
     </Link>
