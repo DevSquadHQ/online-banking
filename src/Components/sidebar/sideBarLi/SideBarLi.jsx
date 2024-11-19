c4a69253c26be4162c78517314b9a81d0bb3d8eb
Alireza Haghighi <alireza.haghighi1224@gmail.com>
2024-11-19 18:23:16 +0330

    fixed: deleted stash lines

--- src/Components/sidebar/sideBarLi/SideBarLi.jsx
+++ src/Components/sidebar/sideBarLi/SideBarLi.jsx
@@ -1,30 +1,26 @@
 import React from "react";
-import './Sidebar.css'
+import "./Sidebar.css";
 import { useNavigate } from "react-router-dom";
-
 
 export default function SideBarLi(props) {
   const { icon, expanded, liName, subIcon, onClick, className, to } = props;
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const hadleCLick = () => {
-    if (onClick) onClick()
-      navigate(to)
-  } 
-  
+    if (onClick) onClick();
+    navigate(to);
+  };
+
   return (
-    <li onClick={hadleCLick} >
-      
-     
-      <span className={`overflow-hidden transition-all text-gray-300  ${
+    <li onClick={hadleCLick}>
+      <span
+        className={`overflow-hidden transition-all text-gray-300  ${
           expanded ? "w-6 ml-2" : "w-0"
-        }`} >
-
-      {icon}
-      
-
+        }`}
+      >
+        {icon}
       </span>
-     
+
       <span
         className={`overflow-hidden transition-all pr-2 ${
           expanded ? "w-52" : "w-0"
@@ -32,8 +28,9 @@ export default function SideBarLi(props) {
       >
         {liName}
       </span>
-      <span className={`text-gray-300 transition-all delay-100 ${className}`} >{subIcon}</span>
-     
+      <span className={`text-gray-300 transition-all delay-100 ${className}`}>
+        {subIcon}
+      </span>
     </li>
   );
 }
