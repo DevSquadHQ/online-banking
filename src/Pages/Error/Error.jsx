181c73c97274677b879e40d6e4258007c8da3e6f
Reza <rtajadod93@gmail.com>
2024-12-02 11:59:16 +0100

    error setup

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -6,7 +6,7 @@ export default function Error() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
       <div className="text-center">
-        <h1 className="text-9xl font-extrabold text-red-600">404</h1>
+        <h1 className="text-9xl font-extrabold text-blue-600">404</h1>
         <p className="text-2xl md:text-3xl font-bold mt-6">
           متاسفانه این صفحه وجود ندارد
         </p>
@@ -15,7 +15,7 @@ export default function Error() {
         </p>
         <button
           onClick={() => navigate("/homedata")}
-          className="mt-6 px-6 py-3 bg-red-600 text-white font-semibold text-lg rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 transition"
+          className="mt-6 px-6 py-3 bg-blue-600 text-white font-semibold text-lg rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 transition"
         >
           برگشت به خانه
         </button>
